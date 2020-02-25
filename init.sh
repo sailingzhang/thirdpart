65356139d7a35d81bfd6594cf0a2031c3696a6cb
sailingzhang <aa582885848@outlook.com>
2020-02-25 16:31:42 +0800

    modify init.sh

--- init.sh
+++ init.sh
@@ -1,1 +1,9 @@
+if [ ${1} -eq 1 ];then
 cat libtensorflow1.15/lib/linux_cpu/libtensorflow.so.1.* > libtensorflow1.15/lib/linux_cpu/libtensorflow.so.1
+fi
+
+
+if [ ${1} -eq 0 ];then
+	rm -rf  libtensorflow1.15/lib/linux_cpu/libtensorflow.so.1
+fi
+
