210b8f3e62e1c202160cec646a0a8fd13d7827f4
sailingzhang <aa582885848@outlook.com>
2020-03-28 22:54:51 +0800

    add grpc_web_plugin and grpcproxy

--- tools/proto_to_code/linux_proto_to_code.sh
+++ tools/proto_to_code/linux_proto_to_code.sh
@@ -1,7 +1,9 @@
-rm *.h *.c
+rm *.h *.c *.js *.ts
 bindir="../../grpc1.27.1/bin"
 ${bindir}/protoc  --grpc_out=. --plugin=protoc-gen-grpc=${bindir}/grpc_cpp_plugin  *.proto
 ${bindir}/protoc  --cpp_out=. *.proto
 ${bindir}/protoc  -I  .  --go_out=plugins=grpc:.  *.proto
 
 python -m grpc_tools.protoc --python_out=. --grpc_python_out=. -I. *.proto
+
+${bindir}/protoc -I=. *.proto   --plugin=protoc-gen-grpc-web=${bindir}/protoc-gen-grpc-web  --js_out=import_style=commonjs,binary:. --grpc-web_out=import_style=typescript,mode=grpcwebtext:.
